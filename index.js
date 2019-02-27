8e3e8b8b886902309b0bb0ff354be2947d4f0160
Kellye Greene <kellyecodes@gmail.com>
2019-02-27 15:04:35 -0500

    Adds solution

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const var1 = 2
-const var2 = 31
-const multiply = var1 * var2
-const random = Math.floor(Math.random() * 1000 ) + 1
-const mod = 16 % 6
-const max = Math.max(5, 10, 15, 20)
+const baseOffset = 1000000000;
+const newID = oldID + baseOffset;
+const ageIsValid = Number.isInteger(currentAge);
+
+let randomNumber = Math.random() * 20
+let randomInteger = Math.floor(randomNumber);
+let randomUserID = randomInteger + baseOffset;
