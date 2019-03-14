fe2903840d9148db4201ee9a261a14446304c286
wulman16 <will.ulman@gmail.com>
2019-03-14 11:23:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const var1 = 31
+const var2 = 2
 
-// Write your code here
+const multiply = var1 * var2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = 10 % 6
+
+const max = Math.max(1, 2, 20, -100000)
